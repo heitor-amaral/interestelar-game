afad39907ac38127596c4f0d6a6f9dacccf99a62
heitor-amaral <heitor.amaral.santos@gmail.com>
2018-04-08 10:49:41 -0300

    Corrido lib

--- lib/texto.h
+++ lib/texto.h
@@ -1,34 +1,8 @@
-#include "texto.h"
-#include <GL/freeglut.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef TEXTO_H
+#define TEXTO_H
 
-void escreveTexto(void* font, char* s, float x, float y) {
-  unsigned int i;
-  glRasterPos2f(x, y);
+void escreveTexto(void* font, char* s, float x, float y);
+void escreveNumero(void* font, int numero, float x, float y);
+void escreveNumeroFloat(void* font, float numero, float x, float y);
 
-  for (i = 0; i < strlen (s); i++) {// menor que
-     glutBitmapCharacter(font, s[i]);
-  }
-}
-
-void escreveNumero(void* font, int numero, float x, float y) {
-  unsigned int i;
-  glRasterPos2f(x, y);
-  char buffer[50];
-  sprintf( buffer, "%d", numero );
-  for (i = 0; i < strlen (buffer); i++) {// menor que
-     glutBitmapCharacter(font, buffer[i]);
-  }
-}
-
-void escreveNumeroFloat(void* font, float numero, float x, float y) {
-  unsigned int i;
-  glRasterPos2f(x, y);
-  char buffer[50];
-  sprintf( buffer, "%f", numero );
-  for (i = 0; i < strlen (buffer); i++) {// menor que
-     glutBitmapCharacter(font, buffer[i]);
-  }
-}
+#endif
